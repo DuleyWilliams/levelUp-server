acc4463e90f12e756261f41d800ed083aa6a79f4
Duley Williams <duleygrafix@gmail.com>
2022-06-01 20:27:29 -0500

    dataCheck

---
+++ levelupapi/models/event_gamer.py
@@ -0,0 +1,10 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from levelupapi.models.event import Event
+from levelupapi.models.gamer import Gamer
+
+
+class EventGamer(models.Model):
+
+    gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
